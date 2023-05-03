6b92655b078dae0d711078e18d8f5bb17f74eda2
Mies van der lippe <miesvanderlippe@gmail.com>
2023-05-03 19:17:16 +0200

    Basic working example

--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ A simple script to get a Python Diff. An experiment using PyGit2.
 
 # Examples 
 This line was changed.
+
+
+``` 
+python3 script.py ./.git 800cde4d7a6dbfea94c9e83e0e4d527d96a87e26 086688590be41370fb120afc3fbe9c5f05b119f9
+``` 
---
+++ script.py
@@ -0,0 +1,29 @@
+from argparse import ArgumentParser
+
+from pygit2 import init_repository, Patch
+
+def compare_and_print(repo_path, commit_a, commit_b):
+
+    git_repo = init_repository(repo_path)
+    diff = git_repo.diff(commit_a, commit_b, context_lines=0, interhunk_lines=0)
+    
+    for obj in diff:
+        if type(obj) == Patch:
+            print(f"Found a patch for file {obj.delta.new_file.path}")
+            for hunk in obj.hunks:
+              for line in hunk.lines:
+                if line.new_lineno == -1: 
+                    print(f"[removal line {line.old_lineno}] {line.content.strip()}")
+                if line.old_lineno == -1: 
+                    print(f"[addition line {line.new_lineno}] {line.content.strip()}")  
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("repo_path")
+    parser.add_argument("commit_a")
+    parser.add_argument("commit_b")
+
+    args = parser.parse_args()
+
+    compare_and_print(args.repo_path, args.commit_a, args.commit_b)
+
